87b5750298c2f4538cab46d48c27783a01f132f3
Ayush Yadav <ayushyadav24nov1234@gmail.com>
2024-06-05 17:13:11 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const mainImg = document.querySelector('#main-img')
+const icon = document.querySelector('.ri-heart-3-fill')
+
+mainImg.addEventListener('dblclick', function(){
+    // After double clicking the icon shows up
+    icon.style.transform = 'translate(-50%, -50%) scale(2)'
+    icon.style.opacity = 0.8
+    // And after 1 second of timeout it will be hidden
+    setTimeout(() => {
+        icon.style.opacity = 0
+    }, 1000) /* 1000 = 1 second */
+});
