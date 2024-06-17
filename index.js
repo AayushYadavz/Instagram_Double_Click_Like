1aac4b8d7c883a7550883e2cb7e53d4ea1a69e9b
Ayush Yadav <ayushyadav24nov1234@gmail.com>
2024-06-17 18:04:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const mainImg = document.querySelector('#main-img')
-const icon = document.querySelector('.ri-heart-3-fill')
+const mainImg = document.querySelector('#main-img');
+const icon = document.querySelector('.ri-heart-3-fill');
 
 mainImg.addEventListener('dblclick', function(){
     // After double clicking the icon shows up
-    icon.style.transform = 'translate(-50%, -50%) scale(2)'
-    icon.style.opacity = 0.8
+    icon.style.transform = 'translate(-50%, -50%) scale(2)';
+    icon.style.opacity = 0.8;
     // And after 1 second of timeout it will be hidden
     setTimeout(() => {
         icon.style.opacity = 0
-    }, 1000) /* 1000 = 1 second */
+    }, 1000); /* 1000 = 1 second */
+    setTimeout(() => {
+        icon.style.transform = 'translate(-50%, -50%) scale(0)'
+    }, 2000);
 });
